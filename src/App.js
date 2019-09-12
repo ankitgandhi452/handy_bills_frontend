546a960f0fbab72857bf7fe0c920b06e86a93af3
Ankit Gandhi <ankitgandhi452@gmail.com>
2019-09-12 18:32:35 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NoSsr from '@material-ui/core/NoSsr';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return [
+    <NoSsr defer={true}>
+      <CssBaseline />
+      <Container maxWidth="md" fixed>
+          
+      </Container>
+    </NoSsr>
+  ];
 }
 
 export default App;
