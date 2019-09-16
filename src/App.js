e5ff1f5573f36e06df3388877a55198b6c034848
Ankit Gandhi <ankitgandhi452@gmail.com>
2019-09-16 18:08:57 +0530

    basic material ui setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NoSsr from '@material-ui/core/NoSsr';
 import React from 'react';
 import './App.css';
-import NoSsr from '@material-ui/core/NoSsr';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
+
 
 
 function App() {
-  return [
+  return (
     <NoSsr defer={true}>
       <CssBaseline />
       <Container maxWidth="md" fixed>
-          
       </Container>
     </NoSsr>
-  ];
+  );
 }
 
 export default App;
